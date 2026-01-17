dee31d2e8900135eeff68f90b30446240a933a44
Victor-Quiceno <vquicenoguerra@gmail.com>
2026-01-17 03:30:08 -0500

    Aprendiendo sobre @ModelAttribute

--- src/main/java/com/primerproyecto/spring/app1/springboot_applications/controllers/EjemploController.java
+++ src/main/java/com/primerproyecto/spring/app1/springboot_applications/controllers/EjemploController.java
@@ -1,8 +1,12 @@
 package com.primerproyecto.spring.app1.springboot_applications.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.primerproyecto.spring.app1.springboot_applications.models.Empleados;
 
@@ -13,9 +17,23 @@ public class EjemploController {
     @GetMapping("/detalles_info")
     public String info(Model model) {
 
-        Empleados empleado1 = new Empleados("Juan", "Pérez", "Calle Falsa 123", "Desarrollador", 30, 5551234, 1);
+        Empleados empleado1 = new Empleados("Juan", "Pérez", "Calle 1 #12", "Desarrollador",
+                                            30, 5551234, 1);
 
         model.addAttribute("Empleado", empleado1);
         return "detalles_info";
     }
+
+    @ModelAttribute("Empleados")
+    public List<Empleados> ListaEmpleados(){
+
+        return Arrays.asList(
+            new Empleados("Ana", "Gómez", "Calle 2 #34", "Diseñadora", 28, 5555678, 2),
+            new Empleados("Luis", "Martínez", "Calle 3 #56", "Analista", 35, 5558765, 3),
+            new Empleados("María", "Rodríguez", "Calle 4 #78", "Contadora", 29, 5554321, 4)
+        );
+        
+    }
+
+
 }
